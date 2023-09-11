360bdfbb10f2f2a010af90e12a6b7c4a3f4797de
fkiptooh <fkiptooh@gmail.com>
2023-09-11 11:30:16 +0300

    BackendRest(AuthController-Login)

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -2,6 +2,37 @@ import express from 'express';
 import { createUser, getUserByEmail } from '../db/users';
 import { random, authentication } from '../helpers';
 
+export const login =async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        const user = getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication((await user).authentication.salt, password);
+
+        if ((await user).authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        (await user).authentication.sessionToken = authentication(salt, (await user)._id.toString());
+
+        res.cookie('BACKEND-REST-API', (await user).authentication.sessionToken, { domain: 'localhost', path: '/'})
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+    
+}
+
 export const register = async(req: express.Request, res: express.Response ) => {
     try {
         const { email, password, username }  = req.body;
