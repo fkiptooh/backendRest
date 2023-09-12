dedf555b3867a17c4859a5c82fcc40ceadafab20
fkiptooh <fkiptooh@gmail.com>
2023-09-12 10:38:24 +0300

    feat(DeleteUser) -controller

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -7,13 +7,17 @@ import { getUserBySessionToken } from '../db/users';
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['BACKEND-REST-API']
+        const existingUser =  getUserBySessionToken(sessionToken);
+        if (!existingUser) {
+            return res.status(404).json({
+                status: 403,
+                message: existingUser
+            });
+        }
         if (!sessionToken) {
             return res.sendStatus(403);
         }
-        const existingUser = await getUserBySessionToken(sessionToken);
-        if (!existingUser) {
-            return res.sendStatus(403);
-        }
+       
 
         merge(req, { identity: existingUser});
 
