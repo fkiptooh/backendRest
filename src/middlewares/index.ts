d6852b754b8a480d2b841e5643cbecf1ef3b7911
fkiptooh <fkiptooh@gmail.com>
2023-09-12 10:48:02 +0300

    feat(isOwner) - delete middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,6 +4,27 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
+export const isOwner =async (req:express.Request, res:express.Response, next:express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity.id') as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.status(403).json({
+                message: 'Unauthorized'
+            });
+        }
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(403);
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['BACKEND-REST-API']
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { getAllUsers } from '../controller/users';
+import { deleteUser, getAllUsers } from '../controller/users';
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
+    router.delete('/users/:id', deleteUser);
 }
